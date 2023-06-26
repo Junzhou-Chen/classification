751473240db21466ebd80fa6c8f783b79e7c8d8d
Junzhou Chen <76274719+Junzhou-Chen@users.noreply.github.com>
2023-06-26 17:23:12 +0800

    Update Convolution_Neural_Network.py

--- Convolution_Neural_Network.py
+++ Convolution_Neural_Network.py
@@ -186,7 +186,7 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logging.info(f'Using device {device}')
     model = resnet50(10)
-    
+    model.to(device=device)
 
     try:
         train_model(
