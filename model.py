90490e1da57c0f0fe2caf08e9e09c179e61721c6
Junzhou-chen <1374709124@qq.com>
2023-06-29 12:38:01 +0800

    version 1

---
+++ model.py
@@ -0,0 +1,43 @@
+from torchvision import models
+from utils.dataloader import dataloaders
+import torch
+import torch.nn as nn
+
+
+def model(model_name:str, stage: str, model_path: str = ''):
+    if model_name == 'resnet50':
+        model = models.resnet50(pretrained=True)
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs, dataloaders['classes'])
+    elif model_name == 'resnet101':
+        model = models.resnet101(pretrained=True)
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs, dataloaders['classes'])
+    elif model_name == 'resnet152':
+        model = models.resnet152(pretrained=True)
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs, dataloaders['classes'])
+    elif model_name == 'alexnet':
+        model = models.alexnet(pretrained=True)
+        num_ftrs = model.classifier[6].in_features
+        model.classifier[6] = nn.Linear(num_ftrs, dataloaders['classes'])
+    elif model_name == 'convnext_tiny':
+        model = models.convnext_tiny(pretrained=True)
+        num_ftrs = model.classifier[2].in_features
+        model.fc = nn.Linear(num_ftrs, dataloaders['classes'])
+    elif model_name == 'convnext_base':
+        model = models.convnext_base(pretrained=True)
+        num_ftrs = model.classifier[2].in_features
+        model.classifier[2] = nn.Linear(num_ftrs, dataloaders['classes'])
+    elif model_name == 'google_net':
+        model = models.googlenet(pretrained=True)
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs, dataloaders['classes'])
+    elif model_name == 'convnext_large':
+        model = models.convnext_large(pretrained=True)
+        num_ftrs = model.classifier[2].in_features
+        model.classifier[2] = nn.Linear(num_ftrs, dataloaders['classes'])
+    if stage == 'test':
+        model.load_state_dict(torch.load(model_path, map_location='cpu'))
+        model.eval()
+    return model
---
+++ utils/dataloader.py
@@ -0,0 +1,46 @@
+from torchvision import datasets, transforms
+import torch
+
+# trainTrans = transforms.Compose([
+#     transforms.Resize([299, 299]),  # resize  长宽不一致
+#     # transforms.CenterCrop(224),  # 在图片的中间区域进行裁剪
+#     transforms.RandomRotation(45),
+#     transforms.RandomHorizontalFlip(),
+#     transforms.ToTensor(),  # 将PIL image转成(c,h,w)的tensor格式，并除255进行归一化处理
+#     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # 逐通道进行标准化，均值变为0，标准差变为1
+# ])
+#
+# dataTrans = transforms.Compose([
+#     transforms.Resize([299, 299]),  # resize  长宽不一致
+#     # transforms.CenterCrop(224),  # 在图片的中间区域进行裁剪
+#     transforms.ToTensor(),  # 将PIL image转成(c,h,w)的tensor格式，并除255进行归一化处理
+#     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])  # 逐通道进行标准化，均值变为0，标准差变为1
+# ])
+
+# 数据路径
+data_dir = r'G:\Dataset\classification'
+train_data_dir = f'{data_dir}/train'
+val_data_dir = f'{data_dir}/val'
+test_data_dir = f'{data_dir}/test'
+
+# 通用数据加载器
+train_dataset = datasets.ImageFolder(train_data_dir)
+val_dataset = datasets.ImageFolder(val_data_dir)
+test_dataset = datasets.ImageFolder(test_data_dir)
+
+image_datasets = dict(train=train_dataset, val=val_dataset, test=test_dataset)
+
+dataloaders = {
+    x: torch.utils.data.DataLoader(
+        dataset=image_datasets[x],
+        batch_size=16,
+        shuffle=True,
+        num_workers=24
+    ) for x in ['train', 'val', 'test']
+}
+
+dataloaders['train_size'] = len(train_dataset)
+dataloaders['val_size'] = len(val_dataset)
+dataloaders['test_size'] = len(test_dataset)
+dataloaders['datasets'] = image_datasets
+dataloaders['classes'] = len(train_dataset.classes)
